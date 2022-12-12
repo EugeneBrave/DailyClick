0953823fe9cdda62added307383c968e21e23bc3
EugeneBrave <eugene.linn@gmail.com>
2022-12-12 11:31:03 +0800

    change fort synology nas

--- baha.py
+++ baha.py
@@ -1,4 +1,5 @@
-import datetime, config, cookies
+import config, cookies
+from datetime import datetime
 from browser import Browser
 from os.path import exists
 from selenium.webdriver.support.ui import WebDriverWait
@@ -13,12 +14,11 @@ def login(browser, username, password):
     wait = browser.wait
     driver.get(config.URL_LOGIN)
     try:
-         wait.until(EC.url_changes(config.URL_LOGIN))
+        wait.until(EC.url_changes(config.URL_LOGIN))
     except:
         pass
     curUrl = driver.current_url
-    print(f"curUrl={curUrl}")
-    if curUrl == config.URL_HOME:
+    if curUrl == config.URL_HOMENoAD or curUrl == config.URL_HOME or curUrl == config.URL_HOME2:
         print("已經登入")
         return True
     else:
@@ -26,10 +26,21 @@ def login(browser, username, password):
         user.send_keys(username)
         pwd = driver.find_element(By.NAME, "password")
         pwd.send_keys(password)
-        pwd.send_keys(Keys.RETURN)
+        pwd.send_keys(Keys.RETURN)	
         try:
+            try:
+                wait.until(EC.url_changes(config.URL_HOME))
+            except:
+                pass
+            try:
+                wait.until(EC.url_changes(config.URL_HOMENoAD))
+            except:
+                pass
+            try:
+                wait.until(EC.url_changes(config.URL_HOME2))
+            except:
+                pass
             wait.until(EC.presence_of_element_located((By.ID, "signin-btn")))
-            
             print("登入成功")
             return True
         except Exception as e:
@@ -37,79 +48,33 @@ def login(browser, username, password):
             return False
 
 
-def signIn(browser):
+def signIn(browser, cookiepath):
     driver = browser.driver
     wait = browser.wait
     try:
         signInBtn = wait.until(
             EC.presence_of_element_located((By.ID, "signin-btn"))
         )
-        print(signInBtn.text)
-        signInBtn.click()
+        if "每日簽到已達成" in signInBtn.text:
+            print("每日簽到已達成")
+        else:
+            try:
+                wait.until(
+                    EC.presence_of_element_located((By.CLASS_NAME, "popup-dailybox__title"))
+                )
+            except:
+                signInBtn.click()
+            driver.get_screenshot_as_file(f"{cookiepath}/Screenshots/success.png")
         return True
     except Exception as e:
-        driver.get_screenshot_as_file("/Screenshots/{today}.png".format(today=datetime.datetime.today))
+        now=datetime.strftime(now,'%Y-%m-%d %H:%M:%S')
+        driver.get_screenshot_as_file(f"{cookiepath}/Screenshots/error_{now}.png")
         print(e)
         return False
 
-
-def rewardDouble(browser):
-    driver = browser.driver
-    wait = browser.wait
-    try:
-        try:
-            wait.until(EC.presence_of_element_located((By.XPATH,"//p[text()='廣告能量補充中']")))
-            elms = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//a[@class='dialogify__close']")))
-            print(elms)
-        except:
-            pass
-        dailyboxBtn = wait.until(
-            EC.presence_of_element_located(
-                (By.XPATH, "//button[contains(@class,'popup-dailybox__btn')]")
-            )
-        )
-        print(dailyboxBtn.text)
-        if dailyboxBtn.is_enabled():
-            dailyboxBtn.click()
-            dialogify = wait.until(
-                EC.presence_of_element_located(
-                    (By.XPATH, "//div[contains(@class,'dialogify__body')]/p")
-                )
-            )
-            print(dialogify.text)
-            if dialogify.text == "是否觀看廣告？":
-                submitBtn = wait.until(
-                    EC.element_to_be_clickable(
-                        (
-                            By.XPATH,
-                            "//button[contains(@class,'btn-primary') and @type='submit']",
-                        )
-                    )
-                )
-                submitBtn.click()
-                try:
-                    adIframe = wait.until(EC.visibility_of_element_located((By.XPATH, "//iframe[@title='3rd party ad content']")))
-                    print(adIframe)
-                    driver.switch_to.frame(adIframe)
-                    rewardResumebutton = driver.find_element(By.CLASS_NAME, "rewardResumebutton")
-                    if rewardResumebutton:
-                        rewardResumebutton.click()
-                        driver.switch_to.default_content()
-                except:
-                    pass
-                sleep(45)
-        else:
-            print("已領取雙倍巴幣")
-    except Exception as e:
-        print("發生不預期錯誤!!")
-        print(e)
-        driver.get_screenshot_as_file("/Screenshots/{today}.png".format(today=datetime.datetime.today))
-
-
 def runBot(args):
-    browser = cookies.loadCookies(Browser("chrome"), args.cookiepath)
+    browser = cookies.loadCookies(Browser(args.webdriver, args.remoteip), args.cookiepath)
     if login(browser, args.username, args.password):
-        if signIn(browser):
-            rewardDouble(browser)
         cookies.saveCookies(browser, args.cookiepath)
+        signIn(browser, args.cookiepath)
     browser.driver.close()
--- config.py
+++ config.py
@@ -2,3 +2,5 @@ TIMEOUT_OPERATION = 5  # 2 mins
 TIMEOUT_SMS_AUTH = 10 * 60  # 10 mins
 URL_LOGIN = "https://user.gamer.com.tw/login.php"
 URL_HOME = "https://www.gamer.com.tw/"
+URL_HOME2 = "https://www.gamer.com.tw/index2.php"
+URL_HOMENoAD = "https://www.gamer.com.tw/index2.php?ad=N"
